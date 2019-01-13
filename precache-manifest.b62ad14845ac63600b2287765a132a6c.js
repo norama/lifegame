b5314cbe8ecedfd5860b00a9818cf5443bd08b7e
Nora Mate <norama.matema@gmail.com>
2019-01-13 03:21:41 +0100

    Updates

---
+++ precache-manifest.b62ad14845ac63600b2287765a132a6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "314944236a51b8e14fd8",
+    "url": "/lifegame/static/js/runtime~main.31494423.js"
+  },
+  {
+    "revision": "295629d33b611393f9f6",
+    "url": "/lifegame/static/js/main.295629d3.chunk.js"
+  },
+  {
+    "revision": "2366ceb32ce07d76f9fa",
+    "url": "/lifegame/static/js/1.2366ceb3.chunk.js"
+  },
+  {
+    "revision": "295629d33b611393f9f6",
+    "url": "/lifegame/static/css/main.e520119f.chunk.css"
+  },
+  {
+    "revision": "2366ceb32ce07d76f9fa",
+    "url": "/lifegame/static/css/1.04c8c4ed.chunk.css"
+  },
+  {
+    "revision": "e4b1c30b0b71b4de34f6d38c6982cf01",
+    "url": "/lifegame/index.html"
+  }
+];
