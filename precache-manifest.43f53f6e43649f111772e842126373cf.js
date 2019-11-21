12eeb0cd985a81632383c42c49fe8f00f9c8af75
Nóra Máté <norama.matema@gmail.com>
2019-11-21 19:18:53 +0100

    Updates

---
+++ precache-manifest.43f53f6e43649f111772e842126373cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65af9da0c4bf42434430f4a39605b942",
+    "url": "/lifegame/index.html"
+  },
+  {
+    "revision": "a1979d3eef47478e9853",
+    "url": "/lifegame/static/css/2.9720967d.chunk.css"
+  },
+  {
+    "revision": "2b3aebf99096ea0705bb",
+    "url": "/lifegame/static/css/main.db2e34f0.chunk.css"
+  },
+  {
+    "revision": "a1979d3eef47478e9853",
+    "url": "/lifegame/static/js/2.f2cd6ecf.chunk.js"
+  },
+  {
+    "revision": "2b3aebf99096ea0705bb",
+    "url": "/lifegame/static/js/main.d2a7f404.chunk.js"
+  },
+  {
+    "revision": "de4b4e548472587626fa",
+    "url": "/lifegame/static/js/runtime-main.51461f9e.js"
+  }
+]);
